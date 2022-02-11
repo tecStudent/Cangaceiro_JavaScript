7f0ab0fafb4dab411806559c0d67625f54d40821
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-02-10 22:03:11 -0300

    Prologo: Era uma vez no Sertao

---
+++ client/app/index.js
@@ -0,0 +1,32 @@
+let campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+    
+]
+
+let tbody = document.querySelector('table tbody')
+
+document.querySelector(".form").addEventListener('submit', (event) =>{
+    event.preventDefault()
+    
+    let tr = document.createElement('tr')
+
+    campos.forEach((campo) =>{
+        let td = document.createElement('td')
+        td.textContent = campo.value
+        tr.appendChild(td)
+    })
+    let tdVolume = document.createElement('td')
+    tdVolume.textContent = campos[1].value * campos[2].value
+    tr.appendChild(tdVolume)
+
+    tbody.appendChild(tr)
+
+    campos[0].value = ''
+    campos[1].value = 1
+    campos[2].value = ''
+
+})
+
+console.log(campos)
