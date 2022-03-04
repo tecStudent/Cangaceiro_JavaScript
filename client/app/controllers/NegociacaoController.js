e5b316348a4f038ae9085e3062d0b2153fb77b4b
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-03-03 22:42:57 -0300

    CAPITULO 3 - NO CANGAÇO, É AÇÃO PARA TODO LADO

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,19 +9,10 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault()
-
-
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     parseInt(this._inputQuantidade.value),
-        //     parseFloat(this._inputValor.value)
-        // )
         let data = new Date(
             ...this._inputData.value
             .split('-')
-            .map((item, indice)=>{
-                return item - indice % 2
-            })
+            .map((item, indice)=> item - indice % 2)
             )
         console.log(data)
     }
