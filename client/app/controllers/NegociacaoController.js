508bc91a7cbad1c58993db2b154fde95bc0d3911
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-03-03 23:09:33 -0300

    4.1 ISOLANDO A RESPONSABILIDADE DE CONVERSÃO DE DATAS

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,11 +9,19 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault()
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice)=> item - indice % 2)
-            )
-        console.log(data)
+
+        let converter = new DateConverter()
+
+        let data = converter.paraData(this._inputData.value)
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+        
+        let diaMesAno = converter.paraTexto(negociacao.data)
+
+        console.log(diaMesAno)
     }
 }
---
+++ client/app/ui/converters/DateConverter.js
@@ -0,0 +1,13 @@
+class DateConverter {
+    paraTexto(data){
+       return data.getDate()
+        +'/'+ (data.getMonth()+1)
+        +'/'+ data.getFullYear()
+    }
+    paraData(texto){
+        return new Date(
+            ...texto.split('-')
+            .map((item,indice) => item - indice % 2)
+        )
+    }
+}
