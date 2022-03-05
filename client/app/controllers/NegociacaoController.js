e759366bf3cb7c4b23f61e3e8ae4cd43a8e86208
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-03-04 22:46:48 -0300

    CAPITULO 5 - O BANDO DEVE SEGUIR UMA REGRA

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,22 +4,27 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
+        this._negociacoes = new Negociacoes()
 
     }
 
-    adiciona(event){
-        event.preventDefault()   
+    _limpaFormulario(){
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
+    }
 
-         
-
-        let negociacao = new Negociacao(
+    _criarNegocicao(){
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
-        )
-        
-        let diaMesAno = DateConverter.paraTexto(negociacao.data)
+            parseFloat(this._inputValor.value))
+    }
 
-        console.log(diaMesAno)
+    adiciona(event){
+        event.preventDefault()        
+        this._negociacoes.adiciona(this._criarNegocicao())
+        this._limpaFormulario()
     }
 }
